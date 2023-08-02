9075573893ac9aaa1d536563877b58fbe4e68e4e
Burrouwill <wgr.burroughs@gmail.com>
2023-08-03 11:34:48 +1200

    itemOperations class implemented

--- IndividualProject/Section A/js/itemoperations.js
+++ IndividualProject/Section A/js/itemoperations.js
@@ -1,21 +1,31 @@
 const itemOperations = {
+    /*Fields*/
     items:[],
+
+    /* adds an item into the array items*/
     add(itemObject){
-        /* adds an item into the array items*/
+       this.items.push(itemObject);
     },
+
+    /* removes the item which has the "isMarked" field set to true*/
     remove(){
-         /* removes the item which has the "isMarked" field set to true*/
+         this.items = this.items.filter(item => !item.isMarked);
    },
+
+    /* searches the item with a given argument id */
     search(id){
-            /* searches the item with a given argument id */
+          return this.items.find(item => item.id == id);  
     },
+
+    /* toggle the isMarked field of the item with the given argument id*/
     markUnMark(id){
-        /* toggle the isMarked field of the item with the given argument id*/
-       
+        const item = this.search(id)
+        item.isMarked = !item.isMarked
 },
-   countTotalMarked(){
-       /* counts the total number of marked items */
-       
+
+    /* counts the total number of marked items */
+    countTotalMarked(){
+       return this.items.filter(item => item.isMarked).length;
 },
    
 }
