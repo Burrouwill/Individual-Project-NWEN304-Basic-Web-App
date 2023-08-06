44a5abcc9eff753fa7b356c09c875fc5b246dad1
Burrouwill <wgr.burroughs@gmail.com>
2023-08-06 19:42:15 +1200

    Created MongoDB. App now connects to db.

--- IndividualProject/Section A/app.js
+++ IndividualProject/Section A/app.js
@@ -22,6 +22,39 @@ const corsOptions = {
 // Enable CORS
 app.use(cors(corsOptions)); // Use this before defining routes
 
+
+/**
+ * MongoDB
+ */
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { default: mongoose } = require('mongoose');
+const uri = "mongodb+srv://admin:admin@cluster0.oifliqb.mongodb.net/node-api?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -53,3 +86,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
